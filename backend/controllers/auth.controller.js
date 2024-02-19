6fe176702661838bb5b1c5d0d2269dcadb80b94d
Akshay <varma.akshay121@gmail.com>
2024-02-20 01:03:30 +0530

    messages section

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,10 +51,38 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("Login user")
+export const login = async(req, res) => {
+    try{
+        const{userName, password} = req.body;
+        const user = await User.findOne({userName});
+
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password||'');
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid username or password"})
+        }
+
+        generateTokenAndSetCookie(user._id,res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            userName: user.userName,
+            profilePic: user.profilePic
+        })
+    }catch(err){
+        console.log("Error in login controller",error.message)
+        res.status(500).json({error:"Internal Server Error"})
+    }
 }
 
-export const logout = (req, res) => {
-    res.send("Logout user")
+export const logout = async (req, res) => {
+    try {
+        res.cookie("jwt","",{
+            maxAge: 0,
+        });
+        res.status(200).json({message:"Logged out successfully"})
+    } catch (error) {
+        console.log("Error in login controller",error.message)
+        res.status(500).json({error:"Internal Server Error"})
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "../backend/authRoutes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDb.js";
 
 const app = express();
@@ -9,8 +11,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); 
+app.use(cookieParser()); 
 
 app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
 
 // app.get("/",(req,res)=>{
 //     res.send("Hello Akshay")
