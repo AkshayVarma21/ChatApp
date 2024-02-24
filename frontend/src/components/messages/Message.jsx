bb9a1f1410cdf2ab0ee6f480db2602f3c579b178
Akshay <varma.akshay121@gmail.com>
2024-02-25 00:29:43 +0530

    UI complete

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,26 @@
+import { useAuthContext } from "../../context/authContext";
+import useConversation from "../../store/useConversation";
+import { extractTime } from "../../utils/extractTime";
 
 
-const Message = () => {
-
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const formattedTime = extractTime(message.createdAt);
+	const isLoggedUser = message.senderId === authUser._id;
+	const chatClassName = isLoggedUser ? "chat-end" : "chat-start";
+	const profilePic = isLoggedUser ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = isLoggedUser ? "bg-blue-500" : "";
 
 	return (
-		<div className='chat chat-end'>
+		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={"https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"} />
+					<img alt='Profile pic' src={profilePic} />
 				</div>
 			</div>
-            <div className={`chat-bubble text-white bg-blue-500`}>Hi whats up</div>
-            <div className='chat-footer text-white opacity-50 text-xs flex gap-1 items-center'>12:10</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+			<div className='chat-footer text-white opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
 		</div>
 	);
 };
---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import useConversation from "../store/useConversation"
+import toast from "react-hot-toast";
+
+const useGetMessages = () => {
+    const [loading, setLoading] = useState()
+    const { messages, setMessages, selectedConversation } = useConversation();
+
+    useEffect(() => {
+
+        const getMessages = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(`/api/messages/${selectedConversation?._id}`);
+                if (res.status === 200 || res.status === 201) {
+                    const data = await res.json();
+                    if (data.error) {
+                        throw new Error(data.error);
+                    }
+                    setMessages(data);
+                }
+
+
+
+            } catch (error) {
+                toast.error(error.message)
+            } finally {
+                setLoading(false);
+            }
+        }
+        if (selectedConversation?._id) {
+            getMessages();
+        }
+
+    }, [selectedConversation?._id, setMessages])
+
+    return { messages, loading }
+}
+
+export default useGetMessages
