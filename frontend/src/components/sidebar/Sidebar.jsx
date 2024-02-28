415c5458fb9cf0664aba744cfa8f327439fd015f
Akshay <varma.akshay121@gmail.com>
2024-02-28 22:53:16 +0530

    responsive changes chat screen

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,12 +4,16 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
 	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-			<Conversations />
-			<LogoutButton />
+		<div id="menu" class='border-r border-slate-500 p-4 flex flex-col'>
+			<button id="hamburger-btn" class="sm:hidden">&#9776;</button>
+			<div id="content" class="hidden sm:block">
+				<SearchInput />
+				<div class='divider px-3'></div>
+				<Conversations />
+				<LogoutButton />
+			</div>
 		</div>
+
 	);
 };
 export default Sidebar;
